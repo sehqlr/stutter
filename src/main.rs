429054764a869dcaad4b0f6017e118bba7b58420
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-06-22 15:54:11 -0500

    Implement echo REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, stutter!")
+    print!("stutter version 0.0.1\n");
+    print!("Press Ctrl+c to exit\n");
+    print!("stutter>");
+
+    loop {
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+
+        println!("No, you're a {}", input);
+        print!("stutter>");
+    }
 }
